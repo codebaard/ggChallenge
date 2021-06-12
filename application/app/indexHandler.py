f9ba6897e6cb34369299af646833e8a75fa7e5a1
codebaard <github@juliusneudecker.com>
2021-06-12 02:09:48 +0200

    added mock response and instructions

--- application/app/indexHandler.py
+++ application/app/indexHandler.py
@@ -1,8 +1,19 @@
 from flask import (
-    Blueprint, render_template,
+    Blueprint, render_template, jsonify
 )
 
 bp = Blueprint('landing', __name__)
+
+@bp.route('/troops')
+def renderJSON():
+    army = {
+        'spearmen': '33',
+        'swordsmen': '33',
+        'archers': '34'
+    }
+
+    return jsonify(army)
+
 
 @bp.route('/')
 def index():
