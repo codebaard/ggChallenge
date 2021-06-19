1cb0db863ec0cffe3af91d0bc3a42e0180bf65c8
codebaard <github@juliusneudecker.com>
2021-06-19 19:03:03 +0200

    prod debug

--- application/app/apiHandler.py
+++ application/app/apiHandler.py
@@ -9,19 +9,14 @@ bp = Blueprint('api', __name__)
 
 @bp.route('/troops')
 def renderJSON():
-    print("is this even rendering?")
     try:
-        print("this 1")
         size = request.args.get('size')
-        print("this 2")
         army = Army(size)
         army.populate()
-        print("this 3")
         resp = make_response(army.toJSON())
         resp.mimetype = 'application/json'
-        resp.status = 200
+        #resp.status = 200
 
-        print("this 4")
         return resp
 
     except Exception as e:
