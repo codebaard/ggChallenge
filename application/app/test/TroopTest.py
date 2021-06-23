a3703fd0ade6ee32d57532f9e90b7cac3f7f1653
codebaard <github@juliusneudecker.com>
2021-06-23 19:21:13 +0200

    TroopTest finished

--- application/app/test/TroopTest.py
+++ application/app/test/TroopTest.py
@@ -1,10 +1,11 @@
 import unittest
-
+from app.model.Troop import Troop
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
-
+    def test_CorrectInitialization(self):
+        testTroop = Troop("Test", 10)
+        self.assertEqual(testTroop.name, "Test")
+        self.assertEqual(testTroop.count, 10)
 
 if __name__ == '__main__':
     unittest.main()
