6e4d9c68ceec680292bffb307e0f788f278347bf
codebaard <github@juliusneudecker.com>
2021-06-24 21:23:02 +0200

    changed port setting

--- build.sh
+++ build.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 echo "BUILD SCRIPT: Script alive!"
-
-git pull
  
 echo "BUILD SCRIPT: building with Dockerfile."
- docker build -t docker.luzifer.cloud/ggchallenge .
+docker build -t ggchallenge .
 
-echo "BUILD SCRIPT: building done. Pushing..."
- 
- docker push docker.luzifer.cloud/ggchallenge
- 
-echo "BUILD SCRIPT: successfully pushed."
+echo "BUILD SCRIPT: building done."
 
 return 0
--- setenv.bat
+++
@@ -1,2 +0,0 @@
-set FLASK_APP=app
-set FLASK_ENV=development
